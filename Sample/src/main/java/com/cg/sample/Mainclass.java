b07896a75b5f49140626a4a4b236a3be3ccfed7a
chandhg <chandhg@DIN78000317.corp.capgemini.com>
2019-07-04 14:18:44 +0530

    this is commit

---
+++ Sample/src/main/java/com/cg/sample/Mainclass.java
@@ -0,0 +1,20 @@
+package com.cg.sample;
+
+public class Mainclass {
+
+		static String breed="abc";
+		String color= "brown";
+		String name="dabour";
+	public static void main(String args[])
+	{
+
+	
+	
+		System.out.println("This is animals class");
+		System.out.println("Dog breed" );
+		System.out.println("Dog color ");
+		System.out.println("Dog name ");
+	}
+	}
+
+
